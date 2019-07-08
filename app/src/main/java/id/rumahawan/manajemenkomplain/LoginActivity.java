75ff1fd58fae94268567309e030d7d21b9ddf286
Hudio Hizari <hhizari@gmail.com>
2019-07-09 03:40:03 +0700

    Perbaikan check login

--- app/src/main/java/id/rumahawan/manajemenkomplain/LoginActivity.java
+++ app/src/main/java/id/rumahawan/manajemenkomplain/LoginActivity.java
@@ -10,14 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.textfield.TextInputLayout;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -54,67 +48,59 @@ public class LoginActivity extends AppCompatActivity {
                 email = tilEmail.getEditText().getText().toString();
             }
             if (tilPassword.getEditText() != null) {
-                password = getMd5(tilPassword.getEditText().getText().toString());
+                password = tilPassword.getEditText().getText().toString();
             }
             if (isValidEmail(email)) {
-                checkLogin(email, password);
+                checkLogin();
             } else {
                 Toast.makeText(LoginActivity.this, "Format email salah", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private String getMd5(String string) {
-        try {
-            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
-            digest.update(string.getBytes());
-            byte[] messageDigest = digest.digest();
-
-            // Create Hex String
-            StringBuilder hexString = new StringBuilder();
-            for (byte aMessageDigest : messageDigest) {
-                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
-                while (h.length() < 2)
-                    h.insert(0, "0");
-                hexString.append(h);
-            }
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+//    private String getMd5(String string) {
+//        try {
+//            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+//            digest.update(string.getBytes());
+//            byte[] messageDigest = digest.digest();
+//
+//            // Create Hex String
+//            StringBuilder hexString = new StringBuilder();
+//            for (byte aMessageDigest : messageDigest) {
+//                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
+//                while (h.length() < 2)
+//                    h.insert(0, "0");
+//                hexString.append(h);
+//            }
+//            return hexString.toString();
+//
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//        return "";
+//    }
 
     private boolean isValidEmail(String email) {
         String regex = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
     }
 
-    private void checkLogin(String email, String password) {
+    private void checkLogin() {
         progressDialog.setMessage("Sedang login...");
         progressDialog.show();
-        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.POST, apiUrl + "?check-login&email=" + email, null, response -> {
-            if (response.length() == 0) {
+        StringRequest request = new StringRequest(Request.Method.POST, apiUrl + "?check-login&email=" + email + "&password=" + password, response -> {
+            if (response.equalsIgnoreCase("empty")){
                 Toast.makeText(this, "Email tidak ditemukan", Toast.LENGTH_SHORT).show();
-            } else {
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject dataUser = response.getJSONObject(i);
-                        if (password.equals(dataUser.getString("password"))) {
-                            session.setSessionString("email", email);
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                            finish();
-                        } else {
-                            Toast.makeText(this, "Password salah", Toast.LENGTH_SHORT).show();
-                        }
-                        progressDialog.dismiss();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
-        }, error -> Log.e("volley", "error : " + error.getMessage()));
-        VolleyController.getInstance().addToRequestQueue(arrayRequest);
+            else if (response.equalsIgnoreCase("ok")){
+                session.setSessionString("email", email);
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+            else if (response.equalsIgnoreCase("deny")){
+                Toast.makeText(this, "Password salah", Toast.LENGTH_SHORT).show();
+            }
+        }, error -> Log.e("volley", "error : " + error));
+        VolleyController.getInstance().addToRequestQueue(request);
     }
 }
